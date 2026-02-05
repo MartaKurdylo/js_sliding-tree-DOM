d444b817070af9187b6e83d908c280e73e9c820c
Marta Kurdylo <martakurdylo17@gmail.com>
2026-02-05 19:57:34 +0200

    code has mistake

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+// li, які мають вкладений ul
+const liItems = tree.querySelectorAll('li:has(> ul)');
+
+liItems.forEach((li) => {
+  const span = document.createElement('span');
+
+  span.textContent = li.firstChild.textContent.trim();
+  li.firstChild.replaceWith(span);
+
+  // 🔥 ВАЖЛИВО: ховаємо ul одразу
+  const childUl = li.querySelector(':scope > ul');
+
+  childUl.classList.add('hidden');
+});
+
+// Делегування кліку
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const li = e.target.closest('li');
+  const childUl = li.querySelector(':scope > ul');
+
+  if (!childUl) {
+    return;
+  }
+
+  childUl.classList.toggle('hidden');
+});
